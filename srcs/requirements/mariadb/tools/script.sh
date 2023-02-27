534ffefcc02b87806aa817cea046f0d92e69895d
Mou-SED <moseddik@student.1337.ma>
2023-02-27 16:47:37 -0500

    perf(mariadb): skip test user

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-	mysql_install_db --user=mysql --datadir=/var/lib/mysql
+	mysql_install_db --user=mysql --datadir=/var/lib/mysql --skip-test-db
 fi
 	mysqld --bootstrap --user=mysql << end
 	FLUSH PRIVILEGES;
-	ALTER USER 'root'@'localhost' IDENTIFIED BY '12345';
-	CREATE DATABASE IF NOT EXISTS $db_name;
-	CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pwd';
-	GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%';
+	ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+	CREATE DATABASE IF NOT EXISTS $DB_NAME;
+	CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PWD';
+	GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
 end
 
 
