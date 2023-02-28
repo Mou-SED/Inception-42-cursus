87e30cbf96db66d5763c2d3729d335c7e5d04994
Mou-SED <moseddik@student.1337.ma>
2023-02-28 16:56:06 -0500

    Add php-fpm + wordpress

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,13 +1,28 @@
 #!/bin/sh
 
-chmod -R 777 /var/www/html
+adduser -S nginx
+addgroup -S nginx
+chown -R nginx:nginx /var/www
 
-cd /var/www/html
+mv /tmp/index.html /var/www/
+
+cd /var/www
+tee info.php << end
+<?php phpinfo(); ?>
+end
+
+mkdir -p /var/www/wordpress
+cd /var/www/wordpress
 
 wp core download  --allow-root
 
-wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PWD --allow-root 
+wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PWD --dbhost=$DB_HOST --allow-root 
 
-wp core install --url=example.com --title=inception --admin_user=supervisor --admin_password=strongpassword --admin_email=moseddik.42.fr --allow-root
+wp core install --url=moseddik.42.fr/wordpress --title=inception --admin_user=supervisor --admin_password=strongpassword --admin_email=test@gmail.com --skip-email --allow-root
 
-wp user create moseddik moseddik.42.fr --porcelain --allow-root
+wp user create $DB_USER test@gmail.com --role=author --user_pass=$DB_PWD --allow-root
+
+wp theme install inspiro --activate --allow-root
+
+
+php-fpm7
