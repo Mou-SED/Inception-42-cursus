0a3dd7a44876213cb4fd844d1dec0d893a96ee45
Mou-SED <moseddik@student.1337.ma>
2023-03-01 10:30:59 -0500

    perf: use env variables

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -18,11 +18,10 @@ wp core download  --allow-root
 
 wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PWD --dbhost=$DB_HOST --allow-root 
 
-wp core install --url=moseddik.42.fr/wordpress --title=inception --admin_user=supervisor --admin_password=strongpassword --admin_email=test@gmail.com --skip-email --allow-root
+wp core install --url=moseddik.42.fr/wordpress --title=$TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PWD --admin_email=$ADMIN_EMAIL --skip-email --allow-root
 
 wp user create $DB_USER test@gmail.com --role=author --user_pass=$DB_PWD --allow-root
 
 wp theme install inspiro --activate --allow-root
-
 
 php-fpm7
